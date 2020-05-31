3c4aaa20e266dbebf95304ee829c28545bc03433
goli8111 <60034176+goli8111@users.noreply.github.com>
2020-05-31 22:54:10 +0900

    okr update

---
+++ QuizData_OKR.js
@@ -0,0 +1,153 @@
+var _quizFullData =
+{
+    "Config": {
+        "quiz_count" : "11",
+        "result_delay_time" : "1",
+        "start_countdown" : "3",
+        "goal_score" : "10",
+        "reward_category" : "wisdom",
+        "reward_exp" : "100"
+    },
+    "Data": [
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "다음 빈 칸에 알맞은 단어는 무엇인가? (pp.35)",
+            "sub_question_1": "(  )는 다름 아닌 성취해야 할 대상이다. (  )는 대단히 중요한 것으로서 구체적이고 행동 지향적(이상적으로)이어야 하며, 영감으로 가득해야 한다. 효과적으로 수립된 (  )는 애매모호한 생각과 행동으로부터 조직을 지켜주는 백신이다.", 
+            "form_type": "radio",
+            "case_1": "핵심결과",
+            "case_2": "OKR",
+            "case_3": "목표",
+            "case_4": "KPI",
+            "right_answer": "3",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "2",
+            "wrong_answer_score" : "0",
+            "question": "다음 중 잘못된 설명을 모두 고르시오. (pp.35)",
+            "form_type": "checkbox",
+            "case_1": "핵심결과는 측정과 검증이 가능해야 한다.",
+            "case_2": "핵심결과는 달성/보류/실패의 세가지 결론에 도달할 수 있다.",
+            "case_3": "핵심결과는 업무 진척도와 무관하게 설정된다.",
+            "case_4": "핵심결과를 모두 달성해도 목표를 달성하지 못할 수도 있다.",
+            "right_answer": "2,3,4",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "2",
+            "wrong_answer_score" : "0",
+            "question": "책에서 소개된 구글의 OKR 활용 예시와 관련하여 잘못 설명한 내용을 모두 고르시오. (pp.44-45)",
+            "form_type": "checkbox",
+            "case_1": "구글이 ‘7의 법칙’을 우회하여 더 많은 인원으로 구성된 조직을 운영할 수 있는 것은 OKR을 활용한 수평 관리 덕분이다.",
+            "case_2": "구글의 OKR은 매분기 인터넷에 투명하게 공개된다.",
+            "case_3": "구글의 개인 OKR은 팀 OKR, 회사 OKR로 연결된다.",
+            "case_4": "구글의 직원들은 다른 직원, 다른 팀의 OKR에 대해 자유롭게 평가할 수 있다.",
+            "right_answer": "2",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "다음 이론 중 OKR과 가장 거리가 먼 것은? (pp.57-61)",
+            "form_type": "radio",
+            "case_1": "피터 드러커의 목표 관리 (MBO; Management by Objectives) 이론",
+            "case_2": "윈슬로 테일러와 헨리 포드의 테일러-포드 모형",
+            "case_3": "앤디 그로브의 인텔목표관리 (iMBO; Intel Management by Objectives) 이론",
+            "right_answer": "2",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "OKR에 대한 다음 설명 중 잘못된 것은? (pp.70-71)",
+            "form_type": "radio",
+            "case_1": "시장 환경이 변하면서 목표가 현실적인 의미를 잃는다면 핵심결과는 얼마든지 수정되거나 폐기될 수 있다.",
+            "case_2": "도전적인 OKR이라 해도 구성원들에게 쉽게 도달할 수 없는 부담감을 주어서는 안된다.",
+            "case_3": "위험 감수를 자극하고 갈등을 예방하기 위해 OKR과 보상은 따로 구분해야 한다.",
+            "case_4": "한가지 목표에 3~5개의 핵심결과가 대응되는 것이 좋다.",
+            "right_answer": "2",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "다음은 중요한 목표를 강조하면서 동시에 품질을 유지하기 위해 핵심결과를 연결한 사례들이다. 다음 중 잘못 연결된 사례는 무엇인가? (pp.93-95)",
+            "form_type": "radio",
+            "case_1": "응용 프로그램에 3가지 신기능 개발 - 품질 테스트에서 기능 당 5가지 미만의 오류",
+            "case_2": "소프트웨어 매출 5000만 달러 - 유지보수 계약 1000만 달러",
+            "case_3": "신형 자동차 모델 2000만원 미만 가격으로 출시 - 무게 0.8톤 미만으로 출시",
+            "right_answer": "3",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "다음 중 OKR의 투명성으로 인한 이점으로 보기 어려운 것은 무엇인가? (pp.126-129)",
+            "form_type": "radio",
+            "case_1": "팀 간 의견 충돌 방지",
+            "case_2": "같은 일이 여러번 수행되는 등의 업무 낭비 감소",
+            "case_3": "개인 목표 달성에 대한 더 많은 동기 부여",
+            "case_4": "구성원 간 논의를 통한 상향식 목표 설정 가능",
+            "right_answer": "1",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "OKR이 하향식 목표 설정 과정으로만 설계될 경우 발생할 수 있는 역효과로 보기 어려운 것은? (pp.135)",
+            "form_type": "radio",
+            "case_1": "OKR 검토 비용이 너무 많이 들어, 분기 별 OKR 수립이 불가능해진다.",
+            "case_2": "사소한 수정만으로도 목표 정렬 비용이 커지는 하위 직급 구성원들의 부담이 커진다.",
+            "case_3": "목표 수립 과정에서 구성원의 조언이나 아이디어 제시가 줄어든다.",
+            "case_4": "구성원 간 기능적인 연결은 강화되나, 수평적인 연결은 위축된다.",
+            "right_answer": "4",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "OKR이 적용된 조직의 운영에 대한 설명으로 잘못된 것은 무엇인가? (pp.136-189)",
+            "form_type": "radio",
+            "case_1": "OKR의 투명성을 위해, 목표 설정 과정에서는 가급적 수직 체계 단계를 지키는 것이 좋다.",
+            "case_2": "구성원 각자가 개인의 목표과 핵심결과의 일부, 혹은 전부를 세우도록 자율권을 허락하는 것이 좋다.",
+            "case_3": "구글의 “20퍼센트 시간” 정책은 하향식 접근 방식과 정반대의 개념이다.",
+            "case_4": "마이크로매니지먼트(micromanagement)는 OKR 과 어울리지 않는다.",
+            "right_answer": "1",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "1",
+            "wrong_answer_score" : "0",
+            "question": "다음 빈 칸에 들어갈 용어를 타이핑하시오. (pp.250)",
+            "sub_question_1": "OKR은 Objective, (   ) Results의 약어이다.",
+            "form_type": "text",
+            "right_answer": "Key, key",
+            "user_answer": "0"
+        },
+        {
+            "time_limit" : "15",
+            "right_answer_score" : "2",
+            "wrong_answer_score" : "0",
+            "question": "OKR의 중간 추적 검토에 따른 액션 플랜에 포함되는 것을 모두 선택하시오. (pp.177-178)",
+            "form_type": "checkbox",
+            "case_1": "새로운 OKR 시작",
+            "case_2": "현재 OKR 중단",
+            "case_3": "목표 수정",
+            "case_4": "핵심 결과 수정",
+            "right_answer": "1,2,3,4",
+            "user_answer": "0"
+        }
+    ]
+}
+
